65bd7738f1a41b3390fd17417de34271e1da4c5e
Sal Alturaigi <saal4106@gmail.com>
2022-10-27 02:31:08 +0300

    Added PharmaSee. Fixed up styles

--- Website Main/html/js/loading.js
+++ Website Main/html/js/loading.js
@@ -1,4 +1,6 @@
 // Very rudimentary for now...
+//
+// DEV-NOTE: Add error message for pages with blocked JavaScript
 
 $(document).ready(function() {
 	document.getElementsByTagName("body")[0].style.visibility = "visible";
